db413f62ab5ec2128609ad94d5275727711d24ca
Nyvi Vieira <Nicollyvieirasc@gmail.com>
2021-07-09 23:13:58 -0300

    mudanças

--- pedido-pizza.py
+++ pedido-pizza.py
@@ -1,19 +1,68 @@
-toppings = []
-if toppings == []:
-    answer = input('Você quer uma pizza simples[N ou S]? ')
-    if answer == 'N':
-        toppings = [input('Adicione um ingrediente: ')]
-        for topping in toppings:
-            print(f'Adicionando {topping}.')
-            answer_2 = input('Quer adicionar mais um ingrediente[N ou S]? ')
-            if answer_2 == 'S':
-                topping = input('Adicione um ingrediente: ')
-                toppings.append(topping)
-            elif answer_2 == 'N':
-                print('OK.')
-        print('\nIngredientes na pizza:')
-        for topping in toppings:
-            print(topping)
-    elif answer == 'S':
-        print('Ok.')
-print('\nPizza finalizada.')
+valores = {'pequena': 20, 'média': 25, 'grande': 30,}
+ingredientes = []
+
+while True: 
+    resposta = input('Gostaria de fazer seu pedido? [S/N] ')
+
+    if resposta.upper() == 'S':
+        while True:
+            opcao = input('Você pode escolher entre "MONTE SUA PIZZA" ou "PIZZA SIMPLES" que vem molho de tomate e queijo. qual você escolhe? ')
+
+            if opcao.lower() == 'monte sua pizza':
+                ingredientes = [input('Qual ingrediente você gostaria de adicionar na sua pizza? ')]
+
+                for ingrediente in ingredientes:
+                    while True:
+                        print(f'Adicionando {ingrediente}.')
+                        resposta = input('\nQuer adicionar mais um ingrediente[N/S]? ')
+
+                        if resposta.upper() == 'S':
+                            ingrediente = input('Digite mais um ingrediente: ')
+                            ingredientes.append(ingrediente)
+                            break
+
+                        elif resposta.upper() == 'N':
+                            print('Ok. Uma pizza com os seguintes ingredientes: ')
+                            break
+                            
+                        else:
+                            print('Erro. Tente novamente.')
+
+                for ingrediente in ingredientes:
+                    print(f'\t- {ingrediente}')
+
+                break
+            elif opcao.lower() == 'pizza simples':
+                print('Ok. Uma pizza simples.')
+                break
+
+            else:
+                print('Erro. Tente novamente.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        break
+
+    elif resposta.upper() == 'N':
+        print(f'Tudo bem. Volte sempre.')
+        break
+
+    else:
+        print('Erro. Tente novamente.')
